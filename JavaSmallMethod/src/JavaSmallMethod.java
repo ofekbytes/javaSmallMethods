3db7c3018bef2130fe598d900c5d7de606514a08
yaron <yaron.kessler41@gmail.com>
2018-06-30 19:40:39 +0300

    test

---
+++ JavaSmallMethod/src/JavaSmallMethod.java
@@ -0,0 +1,16 @@
+
+public class JavaSmallMethod 
+{
+
+	public JavaSmallMethod() 
+	{
+		System.out.println("Java Small Method");
+	}
+
+	public static void main(String[] args) 
+	{
+		JavaSmallMethod javaSmallMethod = new JavaSmallMethod();
+
+	}
+
+}
